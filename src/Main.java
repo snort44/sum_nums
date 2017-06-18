43b88575f8b3e8406611ca92f69aba913117a73c
snort44 <snort44@gmail.com>
2017-06-17 19:55:49 -0600

    another space

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ public class Main {
 //////////////////////////////////////////////////////////////////////
     public static void main(String[] args) {
     int int1 =  3;  //1st number to divide by
-    int int2 = 5;  //2nd number to divide by
+    int int2 =  5;  //2nd number to divide by
     int sum = 0;   //holds the sum
 
     for (int i = 1; i<=1000; i++){
