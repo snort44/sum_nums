3f42a97318915586949215248536cb149f57159f
Art Krahenbuhl <akrahenbuhl@ancestry.com>
2017-06-19 16:17:11 -0600

    Added a calculator assignment

---
+++ src/calculator/Calculator.java
@@ -0,0 +1,19 @@
+package calculator;
+
+/**
+ * This interface defines the "contract" for the Calculator's operations.  You will need to implement this class and provide the
+ * necessary functionality to get the {@link CalculatorAssignment} class working properly.
+ */
+public interface Calculator {
+
+    /*
+    Don't touch this class!
+     */
+
+    Calculator plus(int n);
+
+    Calculator minus(int n);
+
+    int equals();
+
+}
---
+++ src/calculator/CalculatorAssignment.java
@@ -0,0 +1,33 @@
+package calculator;
+
+/**
+ * Get this class running properly by implementing the {@link Calculator} interface.
+ * Requirements:
+ *  - create a new sublcass that implements the Calclulator interface
+ *  -
+ */
+public class CalculatorAssignment {
+
+    public static void main(String[] args) {
+        new CalculatorAssignment().checkAssignment();
+    }
+
+    private void checkAssignment() {
+
+        // uncomment the following and get them working!
+
+//        assertEquals(2, new CalculatorImpl(1).plus(1).equals());
+
+//        assertEquals(9, new CalculatorImpl(5).plus(10).minus(6).equals());
+
+//        assertEquals(29, new CalculatorImpl(0).plus(10).plus(10).plus(10).minus(1).equals());
+
+    }
+
+    private void assertEquals(int expected, int actual) {
+        if (expected != actual) {
+            throw new RuntimeException("keep trying!  " + expected + " != " + actual);
+        }
+    }
+
+}
